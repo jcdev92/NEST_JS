e1c2fcb9c638b2fb16f1578d4f68936f13f2bfe7
Jesus Clemente <jcdeveloper92@gmail.com>
2023-12-28 11:01:37 -0400

    teslo shop database running on docker

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+
+      host: process.env.DB_HOST,
+
+      port: +process.env.DB_PORT,
+
+      database: process.env.DB_DATABASE,
+
+      username: process.env.DB_USERNAME,
+
+      password: process.env.DB_PASSWORD,
+
+      autoLoadEntities: true,
+
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
