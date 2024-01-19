9f71f7cebf574b96d83e5304b6bfa711b0f2b949
Jesus Clemente <jcdeveloper92@gmail.com>
2024-01-19 18:24:09 -0400

    section 15 websockets client and server finish

--- teslo-shop/src/mesaage-ws/mesaage-ws.gateway.ts
+++ teslo-shop/src/mesaage-ws/mesaage-ws.gateway.ts
@@ -1,20 +1,74 @@
 import {
   OnGatewayConnection,
   OnGatewayDisconnect,
+  SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import { MesaageWsService } from './mesaage-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayLoad } from 'src/auth/interfaces';
 
 @WebSocketGateway({ cors: true })
 export class MesaageWsGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
-  constructor(private readonly mesaageWsService: MesaageWsService) {}
-  handleConnection(client: Socket) {
-    console.log('Cliente conectado', client.id);
+  @WebSocketServer() wss: Server;
+  constructor(
+    private readonly mesaageWsService: MesaageWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.authentication as string;
+    let payload: JwtPayLoad;
+    try {
+      payload = this.jwtService.verify(token);
+      await this.mesaageWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    // console.log({ payload });
+    // console.log('Cliente conectado', client.id);
+    this.wss.emit(
+      'clients-updated',
+      this.mesaageWsService.getConnectedClients(),
+    );
   }
+
   handleDisconnect(client: Socket) {
-    console.log('Cliente desconectado', client.id);
+    // console.log('Cliente desconectado', client.id);
+    this.mesaageWsService.removeClient(client.id);
+    this.wss.emit(
+      'clients-updated',
+      this.mesaageWsService.getConnectedClients(),
+    );
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log(payload);
+
+    //? emit only to the emisor
+    // client.emit('message-from-server', {
+    // fullName: this.mesaageWsService.getUserFullName(client.id),
+    // message: payload.message,
+    // )};
+
+    //? for multiple clients except the emisor
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: this.mesaageWsService.getUserFullName(client.id),
+    //   message: payload.message,
+    // });
+
+    //? for all clients
+    this.wss.emit('message-from-server', {
+      fullName: this.mesaageWsService.getUserFullName(client.id),
+      message: payload.message,
+    });
   }
 }
--- teslo-shop/src/mesaage-ws/mesaage-ws.module.ts
+++ teslo-shop/src/mesaage-ws/mesaage-ws.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MesaageWsService } from './mesaage-ws.service';
 import { MesaageWsGateway } from './mesaage-ws.gateway';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [MesaageWsGateway, MesaageWsService],
+  imports: [AuthModule],
 })
 export class MesaageWsModule {}
